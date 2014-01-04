52428b89fb932d9b3530a0d5ec92dc71659a8e11
watsonrp <watsonrp@cpit.ac.nz>
2014-01-04 22:44:56 +1300

    testing 1

---
+++ moodle/recipes/default.rb
@@ -0,0 +1,11 @@
+
+bash "setup_toodle" do
+code <<-EOH
+mkdir /tmp/toodle
+cd /tmp/toodle
+wget https://s3-ap-southeast-2.amazonaws.com/rpw-src/toodle/toodle-site.tgz
+tar xf toodle-site.tgz
+cp -rf * /var/www
+chgrp -R apache /var/www
+chmod -R g+w /var/www
+EOH
