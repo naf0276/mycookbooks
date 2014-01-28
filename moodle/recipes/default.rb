2208c58cb6334a51d83bb7b13907ca832c46b027
watsonrp <watsonrp@cpit.ac.nz>
2014-01-28 22:44:45 +1300

    Update default.rb

--- moodle/recipes/default.rb
+++ moodle/recipes/default.rb
@@ -21,7 +21,7 @@ wget https://s3-ap-southeast-2.amazonaws.com/rpw-src/toodle/toodle-site.tgz
 tar xf toodle-site.tgz
 cp -rf * /var/www
 # mount 172.31.2.24:/moodledata /var/www/moodledata
-s3fs moodlestore /var/www/moodledata
+s3fs s3moodlestore /var/www/moodledata
 chgrp -R apache /var/www
 chmod -R g+w /var/www
 EOH
