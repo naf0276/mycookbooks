0fbdaf0135209c47882259302f8f0b1f84cc7bfd
watsonrp <watsonrp@cpit.ac.nz>
2014-01-28 20:57:33 +1300

    Update default.rb

--- moodle/recipes/default.rb
+++ moodle/recipes/default.rb
@@ -1,15 +1,27 @@
 bash "setup_toodle" do
 code <<-EOH
+yum install gcc libstdc++-devel gcc-c++ fuse fuse-devel curl-devel libxml2-devel openssl-devel mailcap -y
 chkconfig rpcbind on
 chkconfig nfs on
 service rpcbind start
 service nfs start
+mkdir /tmp/s3fs
+cd /tmp/s3fs
+wget http://s3fs.googlecode.com/files/s3fs-1.74.tar.gz 
+tar xvzf s3fs-1.74.tar.gz
+cd s3fs-1.74/
+./configure --prefix=/usr
+make
+make install 
+echo 'AKIAJJOMA3CAE74KMRAQ:2/d78bdutgKOLL7CurywxV9zHGjIZC9S86ZyFjaz' /etc/passwd-s3fs
+chmod 600 /etc/passwd-s3fs
 mkdir /tmp/toodle
 cd /tmp/toodle
 wget https://s3-ap-southeast-2.amazonaws.com/rpw-src/toodle/toodle-site.tgz
 tar xf toodle-site.tgz
 cp -rf * /var/www
-mount 172.31.2.24:/moodledata /var/www/moodledata
+# mount 172.31.2.24:/moodledata /var/www/moodledata
+s3fs moodlestore /var/www/moodledata
 chgrp -R apache /var/www
 chmod -R g+w /var/www
 EOH
