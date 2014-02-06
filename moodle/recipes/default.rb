b4c76ecfa72dab577849bb8e78459b8f731877d1
watsonrp <watsonrp@cpit.ac.nz>
2014-02-07 11:53:31 +1300

    Update default.rb

--- moodle/recipes/default.rb
+++ moodle/recipes/default.rb
@@ -20,9 +20,11 @@ cd /tmp/toodle
 wget https://s3-ap-southeast-2.amazonaws.com/rpw-src/toodle/toodle-site.tgz
 tar xf toodle-site.tgz
 cp -rf * /var/www
+rm -rf /var/www/moodledata
 # mount 172.31.2.24:/moodledata /var/www/moodledata
-s3fs moodlestore /var/www/moodledata
-echo "/var/www/moodledata"
+mkdir /mdata
+s3fs moodlestore /mdata
+ln -s /mdata/moodledata /var/www/moodledata
 chgrp -R apache /var/www
 chmod -R g+w /var/www
 EOH
